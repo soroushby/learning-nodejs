36464739d888cd8106599d6adb0ab78ab82ff2ed
soroushby <49848393+soroushby@users.noreply.github.com>
2020-06-18 04:46:40 -0700

    added model

--- server.js
+++ server.js
@@ -12,13 +12,28 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then((con) => {
-    console.log(con.connections);
-  })
-  .catch((error) => {
-    console.log(error);
+  .then(() => {
+    console.log('successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, ' A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
