0391e8afb3f5f9bdd34604e9e26cf8b024c5ec35
soroushby <49848393+soroushby@users.noreply.github.com>
2020-06-21 04:55:34 -0700

    updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const fs = require('fs');
-const morgan = require('morgan');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const morgan = require('morgan');
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
-    status: 'succes',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    status: 'success',
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -21,17 +18,29 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tour,
-    },
-  });
+  //   const tour = tours.find((el) => el.id === id);
+  //   res.status(200).json({
+  //     status: 'success',
+  //     results: tours.length,
+  //     data: {
+  //       tour,
+  //     },
+  //   });
 };
 
-exports.creatTour = (req, res) => {
-  console.log(req.body);
-  res.send('done');
+exports.creatTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -16,24 +16,6 @@ mongoose
     console.log('successful');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, ' A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
